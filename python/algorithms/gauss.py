dc39ebb9641e4211d9831812e3e9517e19749b27
muslimbeibytuly <muslimbeibytuly@gmail.com>
2019-04-24 21:54:31 +0600

    Add gaussian sum

---
+++ python/algorithms/gauss.py
@@ -0,0 +1,13 @@
+def is_wrong(num):
+    num = int(num)
+    return len({
+        int(num / 100),
+        int((int(num % 100)) / 10), 
+        int(num % 10)
+    }) < 3
+
+    
+def gaussian_sum(l):
+    return sum(x for x in l if not is_wrong(x))
+
+print(gaussian_sum([222, 333, 232, 456, 123, 323]))
