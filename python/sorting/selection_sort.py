69a52492682c3bbde67c253609bb2eb2f81612ff
muslimbeibytuly <muslimbeibytuly@gmail.com>
2019-04-29 21:26:41 +0600

    Add reverted selection sort

--- python/sorting/selection_sort.py
+++ python/sorting/selection_sort.py
@@ -1,9 +1,9 @@
 def selection_sort(l: list) -> None:
     for slot in range(len(l) - 1, 0, -1):
-        max_i = 0
+        min_i = 0
         for temp in range(1, slot + 1):
-            if l[max_i] < l[temp]:
-                max_i = temp
-        temp = l[max_i]
-        l[max_i] = l[slot]
+            if l[min_i] > l[temp]:
+                min_i = temp
+        temp = l[min_i]
+        l[min_i] = l[slot]
         l[slot] = temp
